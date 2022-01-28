61938022a00b6816bf0561bf353442cb179ab2f2
PrestonHarrod <prestonharrod@gmail.com>
2022-01-27 19:05:48 -0600

    Can select your google profile and prints info to console

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-Vue.config.productionTip = false;
+import GoogleAuth from '@/config/google_oAuth.js'
+const gauthOption = {
+  clientId: '738583612295-7lvrgo65m2qnpq05eg20turnoamher1l.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+
+//Vue.config.productionTip = false;
+
+Vue.use(GoogleAuth, gauthOption)
 
 new Vue({
   render: (h) => h(App),
