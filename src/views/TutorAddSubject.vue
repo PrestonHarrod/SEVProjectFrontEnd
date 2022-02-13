11fe5ada0cc0c5999848af66bb6f8964e5f45a07
JackHannahOC <70724833+JackHannahOC@users.noreply.github.com>
2022-02-12 21:54:38 -0600

    Tutor Add Subject page now integrated with backend. Tutors can select/remove subjects from their list of subjects that they can teach

--- src/views/TutorAddSubject.vue
+++ src/views/TutorAddSubject.vue
@@ -3,6 +3,7 @@
       <v-row>
       <v-col>
       <H1 style="background-color: #811429; color: #f2f2f2">Manage Subjects</H1>
+      <br /> 
       </v-col>
       </v-row>
       <v-row>
@@ -10,13 +11,13 @@
       <H2 style="background-color: #811429; color: #f2f2f2"> My Subjects  </H2>
       </v-col>
       </v-row>
-      <br />
-      <div v-for="subject in tutorSubjects" v-bind:key="subject.subjectID">
-        <v-btn v-on:click.prevent="addToSubjects(subjects, tutorSubjects, subject)" rounded color="#811429" dark> {{subject.name}} <v-icon>mdi-close-circle-outline</v-icon> </v-btn>
+      <br /> 
+      <div v-for="subject in mySubjects" v-bind:key="subject.id">
+        <v-btn v-on:click.prevent="moveToSubjects(subjects, mySubjects, subject)" rounded color="#811429" dark> {{subject.name}} <v-icon>mdi-close-circle-outline</v-icon> </v-btn>
       </div>
       <br />
       <br />
-      <!--<v-card width="100vw">
+      <!--<v-card width="100vw">    v-on:click.prevent="addToSubjects(subjects, tutorSubjects, subject)"
         <v-card-title>
           <v-text-field
             v-model="search"
@@ -53,18 +54,22 @@
     <br />
     <br />
     <div v-for="subject in subjects" v-bind:key="subject.subjectID">
-        <v-btn v-on:click.prevent="addToSubjects(tutorSubjects, subjects, subject)" rounded color="#811429" dark> {{subject.name}}  </v-btn>
+        <v-btn v-on:click.prevent="moveToMySubjects(mySubjects, subjects, subject)" rounded color="#811429" dark> {{subject.name}}  </v-btn>
     </div>
   </v-container>
 </template>
 
 <script>
+import Utils from "@/config/utils.js";
+//import UserServices from "@/services/UserServices.js"
 import subjectServices from "@/services/subjectServices.js";
+import tutorSubjectServices from "@/services/tutorSubjectServices";
+
 export default {
   components: {},
   data() {
     return {
-      users: {},
+      user: {},
       search: "",
       headers: [
         {
@@ -85,12 +90,15 @@ export default {
           filterable: true,
         },
       ],
-      subjects: [{}],
+      subjects: [],
       tutorSubjects: [],
+      mySubjects: [],
       selected: [],
     };
   },
   created() {
+    this.user = Utils.getStore('user');
+    console.log(this.user.userID);
     subjectServices
       .getSubjects()
       .then((response) => {
@@ -100,13 +108,59 @@ export default {
         console.log(error);
         alert("ERROR: Retrieve subjects failed.");
       });
+
+      tutorSubjectServices
+      .getTutorSubjectsForTutor(this.user.userID)
+      .then((response) => {
+         response.data.forEach(element => 
+        subjectServices
+        .getSubject(element.subjectID)
+        .then((r) => {
+          this.mySubjects.push(r.data);
+          for(let i = 0; i < this.subjects.length; i++)
+            if(this.subjects[i].subjectID == this.mySubjects[this.mySubjects.length-1].subjectID)
+              this.subjects.splice(i, 1);
+          
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("ERROR: Retrieve subjects failed.");
+        }));
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("ERROR: Retrieve subjects failed.");
+      });
+      console.log("hello hello " + this.tutorSubjects);
   },
   methods: {
     log() {
       console.log(this.selected);
     },
-    addToSubjects(arr, arr2, subject)
+    moveToMySubjects(arr, arr2, subject)
     {
+      console.log(subject);
+      let tSub = {};
+      tSub.tutorID = this.user.userID;
+      tSub.subjectID = subject.subjectID;
+      console.log(tSub);  
+      tutorSubjectServices
+      .addTutorSubject(tSub)
+      .catch((error) => {
+        console.log(error);
+        alert("ERROR: Retrieve subjects failed.");
+      });
+        arr.push(subject);
+        arr2.splice(arr2.indexOf(subject), 1);
+    },
+    moveToSubjects(arr, arr2, subject)
+    {
+      tutorSubjectServices
+      .deleteSpecificTutorSubject(this.user.userID, subject.subjectID)
+      .catch((error) => {
+        console.log(error);
+        alert("ERROR: Retrieve subjects failed.");
+      });
         arr.push(subject);
         arr2.splice(arr2.indexOf(subject), 1);
     },
