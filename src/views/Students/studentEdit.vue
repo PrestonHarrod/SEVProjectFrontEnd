0a8b20544756d1afe6e782ed2c88540e87ff8429
jared <jared.torp@eagles.oc.edu>
2022-03-04 15:14:06 -0600

    work in progress to make the checkboxes clickable

--- src/views/Students/studentEdit.vue
+++ src/views/Students/studentEdit.vue
@@ -39,15 +39,50 @@
             type="text"
             id="phoneNumber"
           />
-          <v-radio-group v-model="student.orgID" column>
-          <v-radio label="Student Success" color="red" value="1"></v-radio>
-          <v-radio
-            label="Writing Center"
-            color="red darken-3"
-            value="2"
-          ></v-radio>
-          <v-radio label="New College" color="indigo" value="3"></v-radio>
-        </v-radio-group>
+
+          <div class="wrap">
+            <div class="left">
+              <h3>Roles:</h3>
+              <ul>
+                <li v-for="role in roles" :key="role.id">
+                  <input
+                    type="checkbox"
+                    :id="role.id"
+                    :value="role.name"
+                    v-model="selectedRoles"
+                  />
+                  <label :for="role.name">{{ role.name }}</label>
+                </li>
+              </ul>
+            </div>
+
+            <div class="right">
+              <div v-for="role in selectedRoles" :key="role.id">
+                {{ role.name }}
+              </div>
+            </div>
+          </div>
+
+          <div class="wrap">
+            <div class="left">
+              <h3>Organizations:</h3>
+              <ul>
+                <li v-for="org in orgs" :key="org.id">
+                  <input
+                    type="checkbox"
+                    :id="org.title"
+                    :value="org.title"
+                    v-model="selectedOrgs"
+                  />
+                  <label :for="org.title">{{ org.title }}</label>
+                </li>
+              </ul>
+            </div>
+
+            <div class="right">
+              <p v-for="org in selectedOrgs" :key="org.id">{{ org.title }}</p>
+            </div>
+          </div>
         </v-col>
         <v-btn
           :style="{ transform: 'translateX(-50%)' }"
@@ -72,6 +107,7 @@
 import UserServices from "@/services/UserServices.js";
 import Utils from "@/config/utils.js";
 import UserOrgServices from "@/services/userOrgServices.js";
+import UserRoleServices from "@/services/userRoleServices.js";
 export default {
   props: ["id"],
   components: {},
@@ -80,6 +116,20 @@ export default {
       user: {},
       student: {},
       menu: false,
+      orgs: [
+        { title: "Student Success Center", id: "1" },
+        { title: "Writing Center", id: 2 },
+        { title: "New College", id: 3 },
+      ],
+      selectedOrgs: [],
+      roles: [
+        { name: "Admin", id: 1 },
+        { name: "Supervisor", id: 2 },
+        { name: "Tutor", id: 3 },
+        { name: "Student", id: 4 },
+        { name: "Mentee", id: 5 },
+      ],
+      selectedRoles: [],
     };
   },
   created() {
@@ -100,21 +150,17 @@ export default {
       this.student.userID = this.id;
       console.log("this is the line of code's student: " + this.student);
       UserServices.updateUser(this.student)
-        .then((response) => {
+        .then(() => {
           this.$router.push({ name: "viewStudent" });
-          var id = response.data.userID;
-          let userOrg = {
-            userID: id,
-            orgID: this.student.orgID,
-          }; //add  for a userOrg
+          // var id = response.data.userID;
 
-          UserOrgServices.updateUserOrg(userOrg)
-            .then(() => {
-              console.log("user org update called...");
-            })
-            .catch((error) => {
-              console.log(error);
-            }); //post the userOrg
+          // UserOrgServices.updateUserOrg(userOrg)
+          //   .then(() => {
+          //     console.log("user org update called...");
+          //   })
+          //   .catch((error) => {
+          //     console.log(error);
+          //   }); //post the userOrg
         })
         .catch((error) => {
           console.log("There was an error:", error.response);
@@ -123,8 +169,67 @@ export default {
           );
         });
     },
+    addUserOrg() {
+      //define the userRole
+      UserRoleServices.addUserRole(userRole)
+        .then(() => {})
+        .catch((error) => {
+          console.log(error);
+        }); //post the user
+
+      
+    },
+    deleteUserOrg() {
+      //deletes a single userOrg Entry when the checkbox is checked
+    },
+    addUserRole() {
+     //define userOrg
+     UserOrgServices.addUserOrg(userOrg)
+        .then(() => {
+          console.log("user org called...");
+        })
+        .catch((error) => {
+          console.log(error);
+        }); //post the userOrg
+    },
+    deleteUserRole() {
+      //deletes a single userRole Entry when the checkbox is checked
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+#app {
+  max-width: 800px;
+  margin: auto;
+}
+
+code {
+  background: #f2f2f2;
+  padding: 0 0.5rem;
+}
+
+.mainBody {
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  margin: 1rem 0;
+  padding: 1rem 0;
+}
+
+.wrap {
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+}
+
+.left,
+.right {
+  flex: 1;
+}
+
+ul {
+  text-align: center;
+  list-style: inside;
+}
+</style>
