708bc4fa5d31d041b98262a44ad7dc3204d09ce8
PrestonHarrod <prestonharrod@gmail.com>
2022-04-11 14:31:51 -0500

    added some more questions in feedback page

--- src/views/Students/studentGiveFeedback.vue
+++ src/views/Students/studentGiveFeedback.vue
@@ -25,22 +25,46 @@
           <br />
           <v-rating
             v-model="rating"
-            background-color="blue lighten-3"
+            background-color="red "
             color="blue"
             large
             @input="getRating()"
           ></v-rating>
+
+          <v-radio-group v-model="question1" column>
+             Did your tutor work with you to identify the topics that would be covered in the session?
+            <v-radio label="Yes" color="red" value="1"></v-radio>
+            <v-radio label="No" color="red darken-3" value="2"></v-radio>
+         </v-radio-group>
+         <v-radio-group v-model="question2" column>
+             Was your tutor on time?
+            <v-radio label="Yes" color="red" value="1"></v-radio>
+            <v-radio label="No" color="red darken-3" value="2"></v-radio>
+         </v-radio-group>
+         <v-radio-group v-model="question3" column>
+             How knowledgeable was the tutor on the subjects discussed?
+            <v-radio label="Outstanding, very knowledgeable" color="red" value="1"></v-radio>
+            <v-radio label="Good, knows most of the material" color="red darken-3" value="2"></v-radio>
+            <v-radio label="Poor, does not know material" color="red darken-3" value="3"></v-radio>
+         </v-radio-group>
+         <v-radio-group v-model="question4" column>
+             How well was your tutor able to explain the material?
+            <v-radio label="Outstanding, it was explain very well" color="red" value="1"></v-radio>
+            <v-radio label="Good, but I still had questions" color="red darken-3" value="2"></v-radio>
+            <v-radio label="Poor, I did not learn much" color="red darken-3" value="3"></v-radio>
+         </v-radio-group>
           <v-textarea
               color="teal"
               v-model="desc"
             >
             <template v-slot:label>
                 <div>
-                  Description
+                  Was your tutor knowledgeable on the topic(s) discussed?
                 </div>
               </template>
               
             </v-textarea>
+            
             <v-btn class='centered-btns' v-if="rating != 0" @click="sendFeedback()" color="primary">
                Submit
             </v-btn>
