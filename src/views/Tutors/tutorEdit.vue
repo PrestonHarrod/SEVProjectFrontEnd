3a23ae7a81031b7a5b6a44c196751f5e891ab079
jared <jared.torp@eagles.oc.edu>
2022-04-11 13:52:04 -0500

    made crud fixes to add multiple orgs and roles in each edit page

--- src/views/Tutors/tutorEdit.vue
+++ src/views/Tutors/tutorEdit.vue
@@ -39,32 +39,60 @@
             type="text"
             id="phoneNumber"
           />
-
-          <v-radio-group v-model="tutor.orgID" column>
-          <v-radio label="Student Success" color="red" value="1"></v-radio>
-          <v-radio
-            label="Writing Center"
-            color="red darken-3"
-            value="2"
-          ></v-radio>
-          <v-radio label="New College" color="indigo" value="3"></v-radio>
-        </v-radio-group>
-        </v-col>
         <v-btn
-        class='centered-btns'
+          :style="{ transform: 'translateX(-50%)' }"
           v-on:click.prevent="updateTutor()"
           text
           rounded
           >Submit</v-btn
         >
         <v-btn
-        class='centered-btns'
+          :style="{ transform: 'translateX(-50%)' }"
           v-on:click.prevent="cancel()"
           color="black"
           text
           rounded
           >Cancel</v-btn
         >
+          <!-- parent -->
+          <div class="float-container">
+            <!-- first -->
+            <div class ="float-child">
+              <div>
+                <h3>Roles:</h3>
+                <ul>
+                  <li v-for="role in roles" :key="role.id">
+                    <input
+                      type="checkbox"
+                      :id="role.id"
+                      :value="role"
+                      v-model="selectedRoles"
+                    />
+                    <label :for="role.name">{{ role.name }}</label>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <!-- second -->
+            <div class ="float-child">
+              <h3>Organizations:</h3>
+              <ul>
+                <li v-for="org in orgs" :key="org.id">
+                  <input
+                    type="checkbox"
+                    :id="org.id"
+                    :value="org"
+                    v-model="selectedOrgs"
+                  />
+                  <label :for="org.title">{{ org.title }}</label>
+                </li>
+              </ul>
+            </div>
+          </div>
+          
+        </v-col>
+
       </v-form>
     </v-app>
   </div>
@@ -73,6 +101,7 @@
 import UserServices from "@/services/UserServices.js";
 import Utils from "@/config/utils.js";
 import UserOrgServices from "@/services/userOrgServices.js";
+import UserRoleServices from "@/services/userRoleServices.js";
 export default {
   props: ["id"],
   components: {},
@@ -81,6 +110,20 @@ export default {
       user: {},
       tutor: {},
       menu: false,
+      orgs: [
+        { title: "Student Success Center", id: "1" },
+        { title: "Writing Center", id: 2 },
+        { title: "New College", id: 3 },
+      ],
+      selectedOrgs: [],
+      roles: [
+        { name: "Admin", id: 1 },
+        { name: "Supervisor", id: 2 },
+        { name: "Tutor", id: 3 },
+        { name: "Student", id: 4 },
+        { name: "Mentee", id: 5 },
+      ],
+      selectedRoles: [],
     };
   },
   created() {
@@ -100,22 +143,65 @@ export default {
     updateTutor() {
       this.tutor.userID = this.id;
 
-      UserServices.updateUser(this.tutor)
-        .then((response) => {
-          this.$router.push({ name: "viewTutor" });
-          var id = response.data.userID;
-          let userOrg = {
-            userID: id,
-            orgID: this.tutor.orgID,
-          }; //add  for a userOrg
+      //delete all the tutor's userRoles with their ID = userID
+      UserRoleServices.deleteUserRole(this.tutor.userID)
+        .then(() => {
+          console.log(
+            "called to delete all the userRoles for user: " +
+              this.tutor.userID
+          );
+        })
 
-          UserOrgServices.updateUserOrg(userOrg)
-            .then(() => {
-              console.log("user org update called...");
-            })
-            .catch((error) => {
-              console.log(error);
-            }); //post the userOrg
+        .catch((error) => {
+          console.log(error);
+        });
+
+      //then add them all back like this
+      for (let i = 0; i < this.selectedRoles.length; i++) {
+        let userRole = {
+          userID: this.tutor.userID,
+          roleID: this.selectedRoles[i].id,
+        };
+        UserRoleServices.addUserRole(userRole)
+          .then(() => {
+            console.log("user role called...");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+
+      //delete all the userOrgs with id = userId
+      UserOrgServices.deleteUserOrg(this.tutor.userID)
+        .then(() => {
+          console.log(
+            "called to delete all the userOrgs for user: " + this.tutor.userID
+          );
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
+
+      //Then add like this
+      for (let i = 0; i < this.selectedOrgs.length; i++) {
+        let userOrg = {
+          userID: this.tutor.userID,
+          orgID: this.selectedOrgs[i].id,
+        };
+        UserOrgServices.addUserOrg(userOrg)
+          .then(() => {
+            console.log("user org called...");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+
+      //finally, update the main components of the tutor
+      UserServices.updateUser(this.tutor)
+        .then(() => {
+          this.$router.push({ name: "viewTutor" });
         })
         .catch((error) => {
           console.log("There was an error:", error.response);
