cc6b7627742ae924ba133b4a4a060169037ed552
JackHannahOC <70724833+JackHannahOC@users.noreply.github.com>
2022-04-15 13:22:59 -0500

    Supervisors now manage subjects for tutors in their org instead of the tutors themselves

--- src/views/subjectList.vue
+++ src/views/subjectList.vue
@@ -1,71 +1,82 @@
 <template>
   <v-container>
-      <v-row>
-        <v-col>
-          <H1>Manage Subjects</H1
-          >
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <H2>
-            My Subjects
-          </H2>
-        </v-col>
-      </v-row>
-      <div v-for="subject in mySubjects" v-bind:key="subject.id">
-        <v-btn
-        class='centered-btns'
-          v-on:click.prevent="moveToSubjects(subjects, mySubjects, subject)"
-          rounded
-          color="#247b7b"
-          dark
-        >
-          {{ subject.name }} <v-icon>mdi-close-circle-outline</v-icon>
-        </v-btn>
-      </div>
+    <v-row>
+      <v-col>
+        <H1>Manage Subjects for Tutor</H1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-combobox
+        @input="changeTutorSubjects"
+        v-model="tutor"
+        :items="tutorList"
+        item-text="name"
+        item-value="userID"
+        label="Tutors"
+      ></v-combobox>
+    </v-row>
+    <v-row>
+      <v-col v-if="tutor.userID != null">
+        <H2> {{tutor.name}}'s Subjects </H2>
+      </v-col>
+      <v-col v-else>
+        <H2> Tutor Subjects </H2>
+      </v-col>
+    </v-row>
+    <div  v-for="subject in mySubjects" v-bind:key="subject.id">
+      <v-btn v-if="tutor.userID != null"
+        class="centered-btns"
+        v-on:click.prevent="moveToSubjects(subjects, mySubjects, subject)"
+        rounded
+        color="#247b7b"
+        dark
+      >
+        {{ subject.name }} <v-icon>mdi-close-circle-outline</v-icon>
+      </v-btn>
+    </div>
 
-      <v-row>
-        <v-col>
-          <H2> Subjects </H2>
-        </v-col>
-      </v-row>
-      <v-row>
-          <v-col>
-              <v-select
-            v-model="selectedSubject"
-            :items="subjectItems"
-            label="Subjects"
-          ></v-select>
-          </v-col>
-          <v-col>
-          <v-select
-            v-model="selectedLevel"
-            :items="levelItems"
-            item-value="levelID"
-            label="Level"
-          ></v-select>
-          </v-col>
-      </v-row>
+    <v-row>
+      <v-col>
+        <H2> Subjects </H2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-select
+          v-model="selectedSubject"
+          :items="subjectItems"
+          label="Subjects"
+        ></v-select>
+      </v-col>
+      <v-col>
+        <v-select
+          v-model="selectedLevel"
+          :items="levelItems"
+          item-value="levelID"
+          label="Level"
+        ></v-select>
+      </v-col>
+    </v-row>
 
-      <div v-for="subject in subjects" v-bind:key="subject.subjectID">
-        <v-btn
-          v-if="
-            (selectedLevel == '' ||
-              selectedLevel == 'Any Level' ||
-              subject.level == selectedLevel) &&
-            (selectedSubject == '' ||
-              selectedSubject == 'Any Subject' ||
-              subject.subjectGenre == selectedSubject)
-          "
-          v-on:click.prevent="moveToMySubjects(mySubjects, subjects, subject)"
-          rounded
-          color="#247b7b"
-          dark
-        >
-          {{ subject.name }}
-        </v-btn>
-      </div>
+    <div v-for="subject in subjects" v-bind:key="subject.subjectID">
+      <v-btn 
+        v-if="
+          (selectedLevel == '' ||
+            selectedLevel == 'Any Level' ||
+            subject.level == selectedLevel) &&
+          (selectedSubject == '' ||
+            selectedSubject == 'Any Subject' ||
+            subject.subjectGenre == selectedSubject)
+          && tutor.userID != null
+        "
+        v-on:click.prevent="moveToMySubjects(mySubjects, subjects, subject)"
+        rounded
+        color="#247b7b"
+        dark
+      >
+        {{ subject.name }}
+      </v-btn>
+    </div>
   </v-container>
 </template>
 
@@ -73,7 +84,8 @@
 import Utils from "@/config/utils.js";
 //import UserServices from "@/services/UserServices.js"
 import subjectServices from "@/services/subjectServices.js";
-import tutorSubjectServices from "@/services/tutorSubjectServices";
+import tutorSubjectServices from "@/services/tutorSubjectServices.js";
+import userServices from "@/services/UserServices.js";
 
 export default {
   components: {},
@@ -104,7 +116,7 @@ export default {
       tutorSubjects: [],
       mySubjects: [],
       selected: [],
-      levelItems: ["Any Level", 1000, 2000, 3000, 4000, 5000],
+      levelItems: ["Any Level", "1000", "2000", "3000", "4000", "5000"],
       selectedLevel: [],
       subjectItems: [
         "Any Subject",
@@ -117,12 +129,54 @@ export default {
         "Nursing",
       ],
       selectedSubject: [],
+      tutorList: [],
+      tutor: "",
     };
   },
   created() {
+    console.log("Org ID: " + Utils.getStore("currentOrg"));
     this.user = Utils.getStore("user");
     console.log(this.user.userID);
-    subjectServices
+    userServices.getUsersByRole(3, Utils.getStore("currentOrg")).then((tutors) => {
+      tutors.data.forEach(tutor => {
+        tutor.name = tutor.fName + " " + tutor.lName;
+        this.tutorList.push(tutor);
+      });
+      
+      
+    });
+  },
+  methods: {
+    log() {
+      console.log(this.selected);
+    },
+    moveToMySubjects(arr, arr2, subject) {
+      //console.log(subject);
+      let tSub = {};
+      tSub.tutorID = this.tutor.userID;
+      tSub.subjectID = subject.subjectID;
+      //console.log(tSub);
+      tutorSubjectServices.addTutorSubject(tSub).catch((error) => {
+        console.log(error);
+        alert("ERROR: Retrieve subjects failed.");
+      });
+      arr.push(subject);
+      arr2.splice(arr2.indexOf(subject), 1);
+    },
+    moveToSubjects(arr, arr2, subject) {
+      tutorSubjectServices
+        .deleteSpecificTutorSubject(this.tutor.userID, subject.subjectID)
+        .catch((error) => {
+          console.log(error);
+          alert("ERROR: Retrieve subjects failed.");
+        });
+      arr.push(subject);
+      arr2.splice(arr2.indexOf(subject), 1);
+    },
+    changeTutorSubjects() {
+      this.subjects = [];
+      this.mySubjects = [];
+      subjectServices
       .getSubjects()
       .then((response) => {
         this.subjects = response.data;
@@ -131,9 +185,8 @@ export default {
         console.log(error);
         alert("ERROR: Retrieve subjects failed.");
       });
-
-    tutorSubjectServices
-      .getTutorSubjectsForTutor(this.user.userID)
+      tutorSubjectServices
+      .getTutorSubjectsForTutor(this.tutor.userID)
       .then((response) => {
         response.data.forEach((element) =>
           subjectServices
@@ -157,34 +210,6 @@ export default {
         console.log(error);
         alert("ERROR: Retrieve subjects failed.");
       });
-    console.log("hello hello " + this.tutorSubjects);
-  },
-  methods: {
-    log() {
-      console.log(this.selected);
-    },
-    moveToMySubjects(arr, arr2, subject) {
-      console.log(subject);
-      let tSub = {};
-      tSub.tutorID = this.user.userID;
-      tSub.subjectID = subject.subjectID;
-      console.log(tSub);
-      tutorSubjectServices.addTutorSubject(tSub).catch((error) => {
-        console.log(error);
-        alert("ERROR: Retrieve subjects failed.");
-      });
-      arr.push(subject);
-      arr2.splice(arr2.indexOf(subject), 1);
-    },
-    moveToSubjects(arr, arr2, subject) {
-      tutorSubjectServices
-        .deleteSpecificTutorSubject(this.user.userID, subject.subjectID)
-        .catch((error) => {
-          console.log(error);
-          alert("ERROR: Retrieve subjects failed.");
-        });
-      arr.push(subject);
-      arr2.splice(arr2.indexOf(subject), 1);
     },
   },
 };
